d735ef227b25b32f1232231d98852190be4c9c10
Mani Vora <voramani.s@gmail.com>
2025-09-29 09:00:03 +0530

    updated backend content

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Middleware
 app.use(express.json()); // IMPORTANT: lets us read req.body
-app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3000' }));
+//app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3000'}));
+app.use(cors({origin: 'https://darling-custard-a00c9d.netlify.app', // your frontend URL
+  credentials: true, // only if using cookies/sessions
+}));
 app.use(morgan('dev'));
 
 // ✅ Routes
